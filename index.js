12309644ef1830bb8d7edb727fb3a3cd444fd1fd
Sujit Akulwar <sujitakulwar@gmail.com>
2022-05-26 23:29:12 -0700

    js 2

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 document.getElementById("img1").style.display = "none";
+document.getElementById("img2").style.display = "none";
+document.getElementById("img3").style.display = "none";
+document.getElementById("img4").style.display = "none";
+document.getElementById("img5").style.display = "none";
+document.getElementById("img6").style.display = "none";
+document.getElementById("img7").style.display = "none";
+document.getElementById("img8").style.display = "none";
+document.getElementById("button3").style.display = "none";
+
+function show(){
+	document.getElementById('button2').style.display = "none";
+	document.getElementById('button3').style.display = "block";
+}
+
+function hide(){
+	document.getElementById('button3').style.display = "none";
+	document.getElementById('button2').style.display = "block";
+}

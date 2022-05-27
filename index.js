9fb9032a51d106d0c3a580ef3e4e7fafdeb8144d
Sujit Akulwar <sujitakulwar@gmail.com>
2022-05-27 12:46:32 -0700

    js css

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function show(){
 }
 
 function hide(){
-	document.getElementById('button3').style.display = "none";
 	document.getElementById('button2').style.display = "block";
+	document.getElementById('button3').style.display = "none";
 }

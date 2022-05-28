69de6df67579c289dbac4c85c8e268b60e5313bc
Sujit Akulwar <sujitakulwar@gmail.com>
2022-05-27 21:49:15 -0700

    sfasd

--- index.js
+++ index.js
@@ -6,13 +6,17 @@ document.getElementById("img5").style.display = "none";
 document.getElementById("img6").style.display = "none";
 document.getElementById("img7").style.display = "none";
 document.getElementById("img8").style.display = "none";
+document.getElementById("hidden").style.display = "none";
+document.getElementById("button3").style.display = "none";
 
 function show(){
 	document.getElementById('button2').style.display = "none";
 	document.getElementById('hidden').style.display = "block";
+	document.getElementById("button3").style.display = "block";
 }
 
 function hide(){
 	document.getElementById('button2').style.display = "block";
 	document.getElementById('hidden').style.display = "none";
+	document.getElementById("button3").style.display = "none";
 }

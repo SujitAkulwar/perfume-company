c4f18319c990e49865733b382cb2052461c41c5b
Sujit Akulwar <sujitakulwar@gmail.com>
2022-05-29 14:35:37 -0700

    aweraewr

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-document.getElementById('img').style.display = "none";
+document.getElementById('1').style.display = "block";
+document.getElementById('2').style.display = "none";
+document.getElementById('3').style.display = "none";
+document.getElementById('4').style.display = "none";
+function left(){
+    let i = 1;
+    
+}

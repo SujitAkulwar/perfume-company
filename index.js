05821cd9ff1b14d2ad40f6561f3dd11be95997b2
Sujit Akulwar <sujitakulwar@gmail.com>
2022-06-16 09:52:03 -0700

    6

--- index.js
+++ index.js
@@ -5,35 +5,25 @@ document.getElementById('4').style.display = "none";
 let i = 1;
 function left(){
     if(i > 1){
-        document.getElementById(i).style.display = "block";
-        i = i - 1;
         document.getElementById(i).style.display = "none";
+        i = i - 1;
+        document.getElementById(i).style.display = "block";
     }
     else{
-        document.getElementById(i).style.display = "block";
-        i = 4;
         document.getElementById(i).style.display = "none";
+        i = 4;
+        document.getElementById(i).style.display = "block";
     }
 }
 function right(){
-    if(i == 1){
-        document.getElementById('1').style.display = "none";
+    if(i < 4){
+        document.getElementById(i).style.display = "none";
         i = i + 1;
-        document.getElementById('2').style.display = "block";
-    }
-    else if(i == 2){
-        document.getElementById('2').style.display = "none";
-        document.getElementById('3').style.display = "block";
-        i = 3;
-    }
-    else if(i == 3){
-        document.getElementById('3').style.display = "none";
-        document.getElementById('4').style.display = "block";
-        i = 4;
+        document.getElementById(i).style.display = "block";
     }
     else{
-        document.getElementById('4').style.display = "none";
-        document.getElementById('1').style.display = "block";
+        document.getElementById(i).style.display = "none";
         i = 1;
+        document.getElementById(i).style.display = "block";
     }
 }
